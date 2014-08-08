8b72afeaf85878d95f4fdc0aeb0901e9dc7c3e8b
Jeado Ko <haibane84@gmail.com>
2014-08-08 16:52:56 +0900

    연재2회 소스 추가

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ angular.module('userMgnt', ['ngCookies','ui.bootstrap']).
  }]).
  controller('mainCtrl', ['$scope','userSvc', function ($scope, userSvc) {
   $scope.userList = userSvc.getUsers();
+  $scope.totalItems = $scope.userList.length;
+  $scope.currentPage = 1;
 
   $scope.insert = function() {
      $scope.userList.push({ edit : true });
