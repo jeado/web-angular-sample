583edb20d2bf534cb33b0ef500e52adb89d8603d
Jeado Ko <haibane84@gmail.com>
2014-10-25 17:46:06 +0900

    폼 벨리데이션 처리 예제 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('serve', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: true,
-      port: 8002	
-    }));
+gulp.task('browser-sync', function() {
+	browserSync.init(null, {
+		notify: false,
+		server: {
+			baseDir: './'
+		},
+		port: 8889
+	});
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("*.html", ['bs-reload']);
+    gulp.watch("*.js", ['bs-reload']);
 });
